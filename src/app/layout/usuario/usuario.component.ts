2ef1424c0d65d5d0f2c498fcb43c648db71e0d97
Ralphcomph <ralphcomph@gmail.com>
2018-07-26 21:50:11 -0300

    datatable

--- src/app/layout/usuario/usuario.component.ts
+++ src/app/layout/usuario/usuario.component.ts
@@ -8,6 +8,24 @@ import { routerTransition } from '../../router.animations';
     animations: [routerTransition()]
 })
 export class UsuarioComponent implements OnInit {
+    
+    settings = {
+        columns: {
+          id: {
+            title: 'ID'
+          },
+          name: {
+            title: 'Full Name'
+          },
+          username: {
+            title: 'User Name'
+          },
+          email: {
+            title: 'Email'
+          }
+        }
+      };
+      
     constructor() {}
 
     ngOnInit() {}
--- src/app/layout/usuario/usuario.module.ts
+++ src/app/layout/usuario/usuario.module.ts
@@ -5,8 +5,10 @@ import { UsuarioRoutingModule } from './usuario-routing.module';
 import { UsuarioComponent } from './usuario.component';
 import { PageHeaderModule } from './../../shared';
 
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+
 @NgModule({
-    imports: [CommonModule, UsuarioRoutingModule, PageHeaderModule],
+    imports: [CommonModule, UsuarioRoutingModule, PageHeaderModule, Ng2SmartTableModule],
     declarations: [UsuarioComponent]
 })
 export class UsuarioModule {}

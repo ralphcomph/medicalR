fc6a0b348d84daa5628538419ea7b2af27cec3ac
Ralphcomph <ralphcomph@gmail.com>
2018-07-27 22:59:40 -0300

    Usuario

--- src/app/layout/usuario/usuario.component.ts
+++ src/app/layout/usuario/usuario.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { routerTransition } from '../../router.animations';
 
+import { LocalDataSource } from 'ng2-smart-table';
+
 @Component({
     selector: 'app-usuario',
     templateUrl: './usuario.component.html',
@@ -10,23 +12,91 @@ import { routerTransition } from '../../router.animations';
 export class UsuarioComponent implements OnInit {
     
     settings = {
-        columns: {
-          id: {
-            title: 'ID'
+      add: {
+        addButtonContent: '<i class="fa fa-plus-square fa-3x"></i>',
+        createButtonContent: '<i class="fa fa-floppy-o">&nbsp;&nbsp;</i>',
+        cancelButtonContent: '<i class="fa fa-ban"></i>',
+      },
+      edit: {
+        editButtonContent: '<i class="fa fa-pencil-square">&nbsp;&nbsp;</i>',
+        saveButtonContent: '<i class="fa fa-floppy-o">&nbsp;&nbsp;</i>',
+        cancelButtonContent: '<i class="fa fa-ban"></i>',
+      },
+      delete: {
+        deleteButtonContent: '<i class="fa fa-trash">&nbsp;&nbsp;</i>',
+        confirmDelete: true,
+      },
+        columns: {         
+          perfil: {
+            title: 'Perfil',
+            filter: false,
+
           },
-          name: {
-            title: 'Full Name'
-          },
-          username: {
-            title: 'User Name'
+          nome: {
+            title: 'Nome',
+            filter: false,
           },
           email: {
-            title: 'Email'
+            title: 'E-mail',
+            filter: false,
+          },
+          senha: {
+            title: 'Senha',
+            filter: false,
           }
         }
       };
-      
-    constructor() {}
 
+      data = [
+        {
+          id: 1,
+          perfil: "Indefinido",
+          nome: "João Augusto",
+          email: "joao@augusto.com.br",
+          senha:"*******"
+        },
+        {
+          id: 2,
+          perfil: "Indefinido",
+          nome: "Maria Aparecida",
+          email: "maria@aparecida.com.br",
+          senha:"*******"
+        },          
+        {
+          id: 3,
+          perfil: "Administrador",
+          nome: "Ralph Souza",
+          email: "ralph@souza.com.br",
+          senha:"*******"
+        }
+      ];
+
+    source: LocalDataSource;
+      
+    constructor() {
+      this.source = new LocalDataSource(this.data);
+    }
+
+    onSearch(query: string = '') {
+      this.source.setFilter([
+        // fields we want to include in the search
+        {
+          field: 'id',
+          search: query
+        },
+        {
+          field: 'name',
+          search: query
+        },
+        {
+          field: 'username',
+          search: query
+        },
+        {
+          field: 'email',
+          search: query
+        }
+      ], false); 
+    }
     ngOnInit() {}
 }
---
+++ src/app/shared/models/usuario.model.ts
@@ -0,0 +1,8 @@
+export class Usuario {
+    public id: number
+    public id_perfil: Array<object>
+    public nome: string
+    public email: string
+    public senha: string
+    public userdel: number  
+}
